8ec0eba98990e12f96c7a0f1791025152b31f308
faiail <raffaele.scaringi20@gmail.com>
2023-06-12 17:43:14 +0200

    add basic metadata construction

---
+++ src/data/get_basic_metadata.py
@@ -0,0 +1,41 @@
+from neo4j import GraphDatabase
+from argparse import ArgumentParser
+import json
+
+
+def get_metadata(driver: GraphDatabase.driver, db: str, metadata: list) -> list:
+    q = [
+        f'match (a:Artwork)--({v["var"]}: {v["class"].capitalize()})' for v in metadata
+    ]
+    q = '\n'.join(q)
+    q += '\nreturn a.name as artwork, '
+    q += ', '.join([
+        f'{v["var"]}.name as {v["class"]}' for v in metadata
+    ])
+
+    with driver.session(database=db) as session:
+        return session.run(q).data()
+
+
+def get_conf():
+    argparser = ArgumentParser()
+    argparser.add_argument('--uri', type=str)
+    argparser.add_argument('--db', type=str)
+    argparser.add_argument('--outpath', type=str)
+    return argparser.parse_args()
+
+
+def launch():
+    args = get_conf()
+    driver = GraphDatabase.driver(uri=args.uri, auth=('neo4j', 'neo4j'))
+    metadata = [
+        {'var': 's', 'class': 'style'},
+        {'var': 'g', 'class': 'genre'},
+    ]
+    data = get_metadata(driver, args.db, metadata)
+    with open(args.outpath, 'w+') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    launch()
