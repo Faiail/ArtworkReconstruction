cf8beb3ef39aaf6b9d1fbe597bef402532f26f04
Faiail <raffaele.scaringi20@gmail.com>
2023-06-13 11:05:34 +0200

    add dataset creation

--- src/data/create_dataset.py
+++ src/data/create_dataset.py
@@ -1,17 +1,18 @@
 import json
 import os
 import re
+import pandas as pd
 from tqdm import tqdm
 from argparse import ArgumentParser
 
-#TODO: AGGIUNGI IL CASO IN CUI VUOI CRARE IL DATASET CON LE CAPTIONS
-
 
 def get_metadata_dataset(metadata_file_path, source_dir):
-    with open(metadata_file_path) as f:
-        metadata = json.load(f)
+
+    metadata=pd.read_json(metadata_file_path).to_dict(orient='records')[:10]
+    #with open(metadata_file_path) as f:
+    #    metadata = json.load(f)
     source_imgs = os.listdir(source_dir)
-    for ix, instance in tqdm(enumerate(metadata[:10])):
+    for ix, instance in tqdm(enumerate(metadata)):
         name = instance['artwork'].split('.')[0]
         references = list(filter(lambda x: x.startswith(name), source_imgs))
         crops = list(filter(lambda x: re.match(f'{name}_crop\d\d\d.png', x) ,references))
@@ -21,17 +22,38 @@ def get_metadata_dataset(metadata_file_path, source_dir):
     return metadata
 
 
+def get_caption_dataset(metadata_file_path, source_dir, captions_file_path, combined=False):
+    metadata = get_metadata_dataset(metadata_file_path, source_dir)
+    captions = pd.read_csv(captions_file_path)
+    captions.index = captions['image']
+    captions.drop(['name', 'image', 'human'], axis=1)
+    for ix, artwork in tqdm(enumerate(metadata)):
+        metadata[ix]['caption'] = captions.loc[artwork["artwork"]].caption
+    if not combined:
+        return [
+            {k: v for k, v in x.items() if k != 'style' and k != 'genre'}
+            for x in metadata
+        ]
+    return metadata
+
+
+
 def parse_args():
     argparser = ArgumentParser()
     argparser.add_argument('--metadatapath', type=str)
     argparser.add_argument('--indir', type=str)
     argparser.add_argument('--outname', type=str)
+    argparser.add_argument('--captionspath', type=str)
+    argparser.add_argument('--combined', type=bool)
     return argparser.parse_args()
 
 
 def launch():
     args = parse_args()
-    metadata = get_metadata_dataset(args.metadatapath, args.indir)
+    if args.captionspath is not None:
+        metadata = get_caption_dataset(args.metadatapath, args.indir, args.captionspath, args.combined)
+    else:
+        metadata = get_metadata_dataset(args.metadatapath, args.indir)
     with open(args.outname, 'w+') as f:
         json.dump(metadata, f)
 
